9b842162e761fc2469ef94c8f3550e47393f8a08
ymka666 <66619758+ymka666@users.noreply.github.com>
2020-12-16 18:52:50 +0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    DATABASE_NAME: 'NakedSun',
+    DATABASE_USERNAME: 'root',
+    DATABASE_PASSWORD: 'iothebest',
+    DATABASE_HOST: 'localhost',
+    DATABASE_DRIVER: 'mysql',
+};
